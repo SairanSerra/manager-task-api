c9e9657b319c4a2ecb3a8acfa4165777a96cc510
Sairan Serra <sairan.serra@gmail.com>
2023-07-19 09:10:15 -0300

    fix - login

--- database/migrations/1689632620906_api_tokens.ts
+++ database/migrations/1689632620906_api_tokens.ts
@@ -10,10 +10,8 @@ export default class extends BaseSchema {
       table.string('name').notNullable()
       table.string('type').notNullable()
       table.string('token', 64).notNullable().unique()
-      table.timestamp('expiresAt').defaultTo(this.now())
-      table.timestamp('createdAt').defaultTo(this.now())
-      table.timestamp('updatedAt').defaultTo(this.now())
-      table.timestamp('deletedAt').nullable()
+      table.timestamp('expires_at', { useTz: true }).nullable().defaultTo(this.now())
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
     })
   }
 
